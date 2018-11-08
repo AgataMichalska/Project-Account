67f1935fdc0d03839ea4473d8de15e970ae494d0
Agata Michalska <agatamichalska1988@gmail.com>
2018-11-08 13:15:52 +0100

    add methods getAllAccounts and getById

--- src/main/java/com/example/Account/Service/AccountService.java
+++ src/main/java/com/example/Account/Service/AccountService.java
@@ -5,10 +5,14 @@ import com.example.Account.Repository.CustomerRepository;
 import com.example.Account.Account;
 import com.example.Account.Repository.AccountRepository;
 import com.example.Account.rest.dto.AccountDTO;
+import com.example.Account.rest.dto.AccountListEntryDTO;
+import com.example.Account.rest.dto.SaveAccountDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class AccountService {
@@ -19,22 +23,41 @@ public class AccountService {
     @Autowired
     private CustomerRepository customerRepository;
 
-    public List<Account> getAllAccounts() {
-        return accountRepository.findAll();
+    public List<AccountListEntryDTO> getAllAccounts() {
+
+        return accountRepository.findAll()
+                .stream()
+                .map(account -> new AccountListEntryDTO(
+                        account.getId(),
+                        account.getAccountName(),
+                        account.getCustomer().getId()))
+                .collect(Collectors.toList());
     }
 
 
-    public void saveAccount(AccountDTO accountDTO) {
+    public void saveAccount(SaveAccountDTO saveAccountDTO) {
 //
-//        Optional<Customer> customerOptional = customerRepository.findById(accountDTO.getCustomerId());
+//        Optional<Customer> customerOptional = customerRepository.findById(saveAccountDTO.getCustomerId());
 //        if (customerOptional.isPresent()) {
-//            Account account = new Account(accountDTO.getAccountName(), customerOptional.get());
+//            Account account = new Account(saveAccountDTO.getAccountName(), customerOptional.get());
 //            accountRepository.save(account);
 //        }
 
-        customerRepository.findById(accountDTO.getCustomerId()).ifPresent(customer -> {
-            Account account = new Account(accountDTO.getAccountName(), customer);
+        customerRepository.findById(saveAccountDTO.getCustomerId()).ifPresent(customer -> {
+            Account account = new Account(saveAccountDTO.getAccountName(), customer);
             accountRepository.save(account);
         });
+    }
+
+    public AccountDTO getById(long accountId) {
+        Optional<Account> accountOptional = accountRepository.findById(accountId);
+        if(accountOptional.isPresent()){
+            Account account = accountOptional.get();
+            return new AccountDTO(account.getId(),
+                    account.getBalance(),
+                    account.getAccountName(),
+                    account.getCustomer());
+        }
+        return null;
     }
 }
