5907824a1022f3b19b50c289ce8a719290a44f6c
Agata Michalska <agatamichalska1988@gmail.com>
2018-11-11 19:41:29 +0100

    create saveAccountDTO

--- src/main/java/com/example/Account/rest/dto/AccountDTO.java
+++ src/main/java/com/example/Account/rest/dto/AccountDTO.java
@@ -1,15 +1,41 @@
 package com.example.Account.rest.dto;
 
+import com.example.Account.Customer;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Getter
 public class AccountDTO {
 
-    private Long customerId;
+    private Long id;
+    private BigDecimal balance;
     private String accountName;
+    private CustomerDTO customer;
 
-    public Long getCustomerId() {
-        return customerId;
+    public AccountDTO(Long id, BigDecimal balance, String accountName, Customer customer) {
+        this.id = id;
+        this.balance = balance;
+        this.accountName = accountName;
+        this.customer = new CustomerDTO(
+                customer.getId(),
+                customer.getName(),
+                customer.getSurname(),
+                customer.getSalary());
     }
 
-    public String getAccountName() {
-        return accountName;
+    @Getter
+    private class CustomerDTO {
+        private Long id;
+        private String name;
+        private String surname;
+        private BigDecimal salary;
+
+        CustomerDTO(Long id, String name, String surname, BigDecimal salary) {
+            this.id = id;
+            this.name = name;
+            this.surname = surname;
+            this.salary = salary;
+        }
     }
 }
---
+++ src/main/java/com/example/Account/rest/dto/SaveAccountDTO.java
@@ -0,0 +1,15 @@
+package com.example.Account.rest.dto;
+
+public class SaveAccountDTO {
+
+    private Long customerId;
+    private String accountName;
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+}
