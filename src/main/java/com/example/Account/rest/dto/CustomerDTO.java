2ae3acefc13d12a777755decd36ca34a64e00e3a
Agata Michalska <agatamichalska1988@gmail.com>
2018-11-11 19:14:54 +0100

    add getRegistrationDateTime()

--- src/main/java/com/example/Account/rest/dto/CustomerDTO.java
+++ src/main/java/com/example/Account/rest/dto/CustomerDTO.java
@@ -1,9 +1,34 @@
 package com.example.Account.rest.dto;
 
-public class CustomerDTO {
+import com.example.Account.Account;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CustomerDTO {
+    private Long id;
     private String name;
     private String surname;
+    private BigDecimal salary;
+    private LocalDateTime registrationDateTime;
+    private List<AccountDTO> accounts;
+
+    public CustomerDTO(Long id, String name, String surname, BigDecimal salary, LocalDateTime registrationDateTime, List<Account> accounts) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.salary = salary;
+        this.registrationDateTime = registrationDateTime;
+        this.accounts = accounts.stream()
+                .map(account -> new CustomerDTO.AccountDTO(account.getId(), account.getBalance(), account.getAccountName()))
+                .collect(Collectors.toList());
+    }
+
+    public Long getId() {
+        return id;
+    }
 
     public String getName() {
         return name;
@@ -11,5 +36,41 @@ public class CustomerDTO {
 
     public String getSurname() {
         return surname;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public LocalDateTime getRegistrationDateTime() {
+        return registrationDateTime;
+    }
+
+    public List<AccountDTO> getAccounts() {
+        return accounts;
+    }
+
+    private class AccountDTO {
+        private Long id;
+        private BigDecimal balance;
+        private String accountName;
+
+        public AccountDTO(Long id, BigDecimal balance, String accountName) {
+            this.id = id;
+            this.balance = balance;
+            this.accountName = accountName;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public BigDecimal getBalance() {
+            return balance;
+        }
+
+        public String getAccountName() {
+            return accountName;
+        }
     }
 }
