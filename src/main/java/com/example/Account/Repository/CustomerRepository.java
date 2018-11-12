5e1b41f9c285c49318462997a03b813fff7d968e
Agata Michalska <agatamichalska1988@gmail.com>
2018-11-12 11:09:56 +0100

    create CustomerRepository

--- src/main/java/com/example/Account/Repository/CustomerRepository.java
+++ src/main/java/com/example/Account/Repository/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.example.Account.Repository;
 
 import com.example.Account.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
 }
