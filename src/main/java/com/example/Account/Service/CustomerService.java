8f0fd2e239b0a22808e2a2943665bb91f1d73ea5
Agata Michalska <agatamichalska1988@gmail.com>
2018-11-13 13:22:49 +0100

    add findAll and getById

--- src/main/java/com/example/Account/Service/CustomerService.java
+++ src/main/java/com/example/Account/Service/CustomerService.java
@@ -4,10 +4,15 @@ package com.example.Account.Service;
 import com.example.Account.Customer;
 import com.example.Account.Repository.CustomerRepository;
 import com.example.Account.rest.dto.CustomerDTO;
+import com.example.Account.rest.dto.CustomerListEntryDTO;
+import com.example.Account.rest.dto.SaveCustomerDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CustomerService {
@@ -15,16 +20,43 @@ public class CustomerService {
     @Autowired
     private CustomerRepository customerRepository;
 
-    public void saveCustomer(CustomerDTO customerDTO) {
-        Customer customer = new Customer(customerDTO.getName(), customerDTO.getSurname());
+    public void saveCustomer(SaveCustomerDTO saveCustomerDTO) {
+        Customer customer = new Customer(saveCustomerDTO.getName(), saveCustomerDTO.getSurname());
         customerRepository.save(customer);
     }
 
-    public List<Customer> getAllCustomers() {
-        return customerRepository.findAll();
+    public List<CustomerListEntryDTO> getAllCustomers() {
+
+//        List<CustomerListEntryDTO> customerListEntryDTOS = new ArrayList<>();
+//        List<Customer> customers = customerRepository.findAll();
+//        for (Customer c : customers) {
+//            customerListEntryDTOS.add(new CustomerListEntryDTO(c.getId(),c.getName(),null));
+//        }
+//        return customerListEntryDTOS;
+
+        return customerRepository.findAll()
+                .stream()
+                .map(customer -> new CustomerListEntryDTO(
+                        customer.getId(),
+                        customer.getName(),
+                        customer.getAccounts()
+                                .stream()
+                                .map(account -> account.getId()).collect(Collectors.toList())))
+                .collect(Collectors.toList());
     }
 
-    public Customer getById(long customerId) {
-        return customerRepository.findById(customerId).orElseGet(null);
+    public CustomerDTO getById(long customerId) {
+        Optional<Customer> customerOptional = customerRepository.findById(customerId);
+        if (customerOptional.isPresent()) {
+            Customer customer = customerOptional.get();
+            return new CustomerDTO(customer.getId(),
+                    customer.getName(),
+                    customer.getSurname(),
+                    customer.getSalary(),
+                    customer.getRegistrationDateTime(),
+                    customer.getAccounts());
+
+        }
+        return null;
     }
 }
